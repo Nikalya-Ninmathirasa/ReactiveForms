a5c041bf43d09de83a1357edcf3cd325286bd986
Nikalya_1398 <v.ninmathirasa@gmail.com>
2024-06-22 16:04:12 +0530

    uploaded files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Routing';
+  title = 'Reactive Forms';
+
+  details={name:"",password:"",email:""};
+
+  register = new FormGroup({
+     name:new FormControl("",Validators.required),
+     password:new FormControl("",Validators.minLength(5)),
+     email:new FormControl("",Validators.email)
+  });
+
+  getData(){
+this.details.name = this.register.value.name;
+this.details.password=this.register.value.password;
+this.details.email=this.register.value.email;
+    
+  }
+
+  get vname(){
+    return this.register.get("name");
+  }
+
+  get vpwrd(){
+    return this.register.get("password")
+  }
+
+  get vemail(){
+    return this.register.get("email")
+  }
+  
 }
